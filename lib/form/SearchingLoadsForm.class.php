1f2d0e689b2f7214a339919620e2b3f4405de639
letuananha3 <letuananha3@gmail.com>
2012-06-04 10:06:14 +0000

    add validator to Form

--- lib/form/SearchingLoadsForm.class.php
+++ lib/form/SearchingLoadsForm.class.php
@@ -1,6 +1,38 @@
 <?php
 class SearchingLoadsForm extends LoadsForm
 {
+	public function configure()
+	{
+		$this->setWidgets(array(
+			'jobboard_id'				=>		new sfWidgetFormChoice(array('choices' => $this->getJobboards())),
+			'origin'					=>		new sfWidgetFormInput(),
+			'origin_radius'				=>		new sfWidgetFormInput(),
+			'destination'				=>		new sfWidgetFormInput(),
+			'destination_radius'		=>		new sfWidgetFormInput(),		
+		));
+		
+		$this->setValidators(array(
+			'jobboard_id'				=>		new sfValidatorChoice(array('choices' => array_keys($this->getJobboards()), 'required' => false)),
+			'origin'					=> 		new sfValidatorString(array('required' => false)),
+			'origin_radius'				=>		new sfValidatorNumber(array('required' => false)),
+			'destination'				=>		new sfValidatorString(array('required' => false)),
+			'destination_radius'		=>		new sfValidatorNumber(array('required' => false)),
+		));
+		
+		$this->widgetSchema->setNameFormat('loads[%s]');
+		$this->widgetSchema->setLabels(array(
+			'jobboard_id'				=>		'Website',
+			'origin'					=>		'Origin',
+			'origin_radius'				=>		'Origin Radius',
+			'destination'				=>		'Destination',
+			'destination_radius'		=>		'Destination Radius',
+		));
+	}
 	
+	
+	private function getJobboards()
+	{
+		return array();
+	}
 }
 ?>
