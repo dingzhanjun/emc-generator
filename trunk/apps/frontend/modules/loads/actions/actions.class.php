e9fbebab943921f89f1e9dd1ded89f3cff5f49b7
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-04 10:05:44 +0000

    update loads module

--- trunk/apps/frontend/modules/loads/actions/actions.class.php
+++ trunk/apps/frontend/modules/loads/actions/actions.class.php
@@ -17,6 +17,50 @@ class loadsActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+	$this->loads_form = new SearchingLoadsForm();
+	$q = Doctrine_Query::create()
+		->from('Loads l');
+		
+	if ($request->hasParameter('loads')) {
+		$form_data = $request->getParameter('loads');
+		$this->loads_form->bind($form_data);
+		$q = $this->executeFilters($form_data, $q);
+	}
+	
+	$this->sort_by = $request->getParameter('sort_by', 'created_at');
+    if ($request->getParameter('sort_order', 1))
+        $this->sort_order = 'desc';
+    else
+        $this->sort_order = 'asc';
+    if (Doctrine_Core::getTable('Loads')->hasField($this->sort_by))
+        $q->orderBy('l.'.$this->sort_by.' '.strtoupper($this->sort_order));
+
+	//echo $q->getSQLQuery();
+	$this->pager = new sfDoctrinePager('Loads', 20);
+	$this->pager->setQuery($q);
+    $this->pager->setPage($request->getParameter('page', 1));
+    $this->pager->init();
+
+	if ($request->isXmlHttpRequest()) {
+        return 'Ajax';
+    }
+    return sfView::SUCCESS;
+  }
+
+ 
+  private function executeFilters($form_data, $q)
+  {
+	if (!empty($form_data['jobboard_id']))
+		$q->addWhere('l.jobboard_id = ?', $form_data['jobboard_id']);
+	if (!empty($form_data['origin']))
+		$q->addWhere('l.origin LIKE "%'.$form_data['origin'].'%"');
+	if (!empty($form_data['origin_radius']))
+		$q->addWhere('l.origin_radius <= ?', $form_data['origin_radius']);
+	if (!empty($form_data['destination']))
+		$q->addWhere('l.destination LIKE "%'.$form_data['destination'].'%"');
+	if (!empty($form_data['destination_radius']))
+		$q->addWhere('l.destination_radius <= ?', $form_data['destination_radius']);
+		
+	return $q;
   }
 }
---
+++ trunk/apps/frontend/modules/loads/templates/indexSuccess.php
@@ -0,0 +1,57 @@
+<script>
+<!--
+var sort_by = '';
+var sort_order = 0;
+var current_view = '';
+var page = 1;
+
+function listReload(page)
+{
+    $('#indexAjax').html('<div align="center"><div class="loader_admin"></div></div>');
+    $('#indexAjax').load('<?php echo url_for('@loads') ?>?'+$('#filterSearch').serialize(), {
+        sort_by:      sort_by,
+        sort_order:   sort_order,
+        page:         page,
+        view:         current_view,
+    });
+}
+
+function sort(type, default_order) {
+    if (type != sort_by) {
+        sort_by = type;
+        sort_order = default_order;
+    } else if(sort_order) {
+        sort_order = 0
+    } else {
+        sort_order = 1;
+    }
+    listReload(1);
+}
+
+/*$(function() {
+		$( "#ground_order_reserved_date" ).datepicker({
+			showOn: "button",
+			minDate: new Date(),
+		});
+	});*/
+-->
+</script>
+<h1>Loads list</h1>
+<div id="SearchForm" class='backend_form'>
+  <form id="filterSearch" action="<?php echo url_for('@loads') ?>" method="post">
+    <table>
+      <?php echo $loads_form ?>
+      <tr><th>
+        <input  id="fs" type="submit" value="<?php echo "Search" ?>"/>
+      </th></tr>
+    </table>
+  </form>
+</div>
+      
+<div style='padding-top:10px;'>
+<div class="UITable">
+  <div id="indexAjax" class='filter_form'>
+    <?php include 'indexAjax.php' ?>
+  </div>
+</div>
+</div>
