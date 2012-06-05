fb9086f398f5821f0dc606e6ce3db41c21d70159
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-05 03:07:11 +0000

    update base class

--- trunk/lib/filter/doctrine/base/BaseLoadsFormFilter.class.php
+++ trunk/lib/filter/doctrine/base/BaseLoadsFormFilter.class.php
@@ -15,7 +15,6 @@ abstract class BaseLoadsFormFilter extends BaseFormFilterDoctrine
     $this->setWidgets(array(
       'web_reference'      => new sfWidgetFormFilterInput(),
       'jobboard_id'        => new sfWidgetFormFilterInput(array('with_empty' => false)),
-      'age'                => new sfWidgetFormFilterInput(),
       'date'               => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'truck_type'         => new sfWidgetFormFilterInput(array('with_empty' => false)),
       'loads_type'         => new sfWidgetFormFilterInput(),
@@ -33,7 +32,6 @@ abstract class BaseLoadsFormFilter extends BaseFormFilterDoctrine
     $this->setValidators(array(
       'web_reference'      => new sfValidatorPass(array('required' => false)),
       'jobboard_id'        => new sfValidatorSchemaFilter('text', new sfValidatorInteger(array('required' => false))),
-      'age'                => new sfValidatorPass(array('required' => false)),
       'date'               => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDateTime(array('required' => false)))),
       'truck_type'         => new sfValidatorPass(array('required' => false)),
       'loads_type'         => new sfValidatorPass(array('required' => false)),
@@ -68,7 +66,6 @@ abstract class BaseLoadsFormFilter extends BaseFormFilterDoctrine
       'hash'               => 'Text',
       'web_reference'      => 'Text',
       'jobboard_id'        => 'Number',
-      'age'                => 'Text',
       'date'               => 'Date',
       'truck_type'         => 'Text',
       'loads_type'         => 'Text',
