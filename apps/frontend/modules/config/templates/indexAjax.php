26f946926c7b6db60ea3eb17cca0b4c25f771aec
letuananha3 <letuananha3@gmail.com>
2012-06-08 11:30:56 +0000

    adding config module

---
+++ apps/frontend/modules/config/templates/indexAjax.php
@@ -0,0 +1,65 @@
+<?php use_helper('Url') ?>
+<?php if (isset($pager)): ?>
+  <table cellspacing="0" class="root_table">
+    <tbody>
+      <tr>
+		  
+        <th>
+			<?php echo link_to_function('<span>Website</span>', 'sort(\'jobboard_id\', 1)') ?>
+        </th>
+		<th>
+           	<?php echo link_to_function('<span>Max Loads Age</span>', 'sort(\'max_age\', 1)') ?>
+        </th>
+		<th>
+           	<?php echo link_to_function('<span>Origin</span>', 'sort(\'origin\', 1)') ?>
+        </th>
+			<th>
+	           	<?php echo link_to_function('<span>Origin Radius</span>', 'sort(\'origin_radius\', 1)') ?>
+	        </th>
+			<th>
+	           	<?php echo link_to_function('<span>Destination</span>', 'sort(\'destination\', 1)') ?>
+	        </th>
+		<th>
+           	<?php echo link_to_function('<span>Destination Radius</span>', 'sort(\'destination_radius\', 1)') ?>
+        </th>
+		<th>
+            <?php echo link_to_function('<span>Frequence</span>', 'sort(\'frequence\', 1)') ?>
+        </th>
+      </tr>
+
+	<?php foreach ($pager->getResults() as $config) :?>
+    <tr>
+        <td style="text-align:center"><?php $jobboard_configs = $config->JobboardConfigs; foreach ($jobboard_configs as $jobboard_config) echo $jobboard_config->Jobboard->name.', ' ?></td>
+		<td style="text-align:center"><?php echo $config->max_age ?></td>
+		<td style="text-align:center"><?php echo $config->origin ?></td>
+		<td style="text-align:center"><?php echo $config->origin_radius ?></td>
+		<td style="text-align:center"><?php echo $config->destination ?></td>
+		<td style="text-align:center"><?php echo $config->destination_radius ?></td>
+		<td style="text-align:center"><?php echo $config->frequence ?></td>
+    </tr>
+  <?php endforeach ?>
+    </tbody>
+  </table>
+
+  <?php if ($pager->haveToPaginate()): ?>
+    <div class="pagination">
+      <?php if (!$pager->isFirstPage()): ?>
+        <?php echo link_to_function("First page", 'listReload('.$pager->getFirstPage().')', array('class' => 'page')) ?>
+        <?php echo link_to_function("Prev", 'listReload('.$pager->getPreviousPage().')', array('class' => 'page')) ?>
+      <?php endif ?>
+
+      <?php foreach ($pager->getLinks() as $link): ?>
+        <?php if ($link == $pager->getPage()): ?>
+          <span class="page selected"><?php echo $link ?></span>
+        <?php else: ?>
+          <?php echo link_to_function($link, 'listReload('.$link.')', array('class' => 'page')) ?>
+        <?php endif ?>
+      <?php endforeach ?>
+
+      <?php if (!$pager->isLastPage()): ?>
+        <?php echo link_to_function("Next", 'listReload('.$pager->getNextPage().')', array('class' => 'page')) ?>
+        <?php echo link_to_function("Last page", 'listReload('.$pager->getLastPage().')', array('class' => 'page')) ?>
+      <?php endif ?>
+    </div>
+  <?php endif ?>
+<?php endif ?>
