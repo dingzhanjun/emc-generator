70e0923c8acfd38c1ccc8539f204b388daa52ec5
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-12 06:54:55 +0000

    new base class

---
+++ trunk/lib/filter/doctrine/TruckFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Truck filter form.
+ *
+ * @package    emc
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class TruckFormFilter extends BaseTruckFormFilter
+{
+  public function configure()
+  {
+  }
+}
