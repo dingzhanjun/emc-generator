3a11f4b12b65c92d48d473026811412c74395e9f
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-16 04:03:33 +0000

    From now a generator can be called from both sf Task or action

--- trunk/lib/task/TruckersedgeGeneratorTask.class.php
+++ trunk/lib/task/TruckersedgeGeneratorTask.class.php
@@ -13,12 +13,8 @@ class TruckersedgeGeneratorTask extends sfBaseTask
 
     $this->addArgument('config', sfCommandArgument::REQUIRED, 'The config id');
   }
-
-  public function create_log($filename, $content) {
-	$file = dirname(dirname(dirname(__FILE__))).'/log/'.$filename;
-	file_put_contents($file, $content);
-  }
  
+  
   public function execute($arguments = array(), $options = array())
   {
     // initialize database connection  
@@ -30,111 +26,11 @@ class TruckersedgeGeneratorTask extends sfBaseTask
 
     // getting config
 	$config_id = $arguments['config'];
-	$config = Doctrine_Core::getTable('Config')->find($config_id);
-	if (!$config) {
-		$this->logSection('Error', 'Config not found');
-		exit;
-	}
-
-	$this->logSection('info', 'Executing generator with config: '.$config->id);
-    $client->get('https://www.truckersedge.net/a/secure/login.aspx?app=truckersedge');
-    $client->load(array('id' => 'aspnetForm', 'name' => 'aspnetForm'));
-    $client->validate(array(
-                    '__LASTFOCUS'                        => 'input-hidden',
-                    'VAM_Group'                          => 'input-hidden',
-                    '__VIEWSTATE'                        => 'input-hidden',
-                    '__EVENTTARGET'                      => 'input-hidden',
-                    '__EVENTARGUMENT'                    => 'input-hidden',
-                    '__EVENTVALIDATION'                  => 'input-hidden',
-                    'ctl00$cphMain$txtUserName'          => 'input-text',
-                    'ctl00$cphMain$txtPassword'          => 'input-password',
-                    'ctl00$cphMain$chkRememberIdentity'  => 'input-checkbox',
-                    'ctl00$cphMain$btnSubmit'            => 'input-submit',
-                    'NoJS'                               => 'input-hidden',
-                    'ctl00$cphMain$hfUserTimezoneOffset' => 'input-hidden',
-                    ));
-    $client->removeField('NoJS');
-    $tag['login'] = $client->getData();
-	$jobboard = Doctrine_Core::getTable('Jobboard')->findOneByName('Truckersedge');
-    $tag['login']['ctl00$cphMain$txtUserName'] = $jobboard->username;
-    $tag['login']['ctl00$cphMain$txtPassword'] = $jobboard->password;
-    $tag['login']['ctl00$cphMain$hfUserTimezoneOffset'] = -420;
-    $client->fill($tag['login']);
-    $client->post('https://www.truckersedge.net/a/secure/login.aspx?app=truckersedge&');
-    $client->get('http://www.truckersedge.net/a/app/default.aspx');
-    $this->create_log('login'.date(DATE_ISO8601).'.html', $client->getBody());
-    if (!preg_match('#TruckersEdge.net - My Overview#', $client->getBody(), $match)) {
-        $this->logSection('info', "Login fail");
-        exit;
-    }
-    $this->logSection('info', 'login success !!! Redirecting to seaching page');
-
-    $client->get('http://www.truckersedge.net/a/app/Search.aspx');
-	$this->create_log('searching_page'.date(DATE_ISO8601).'.html', $client->getBody());
-	$client->load(array('id' => 'aspnetForm', 'name' => 'aspnetForm'));
-	$client->validate(array(
-	                '__EVENTTARGET'                                             => 'input-hidden',
-	                '__EVENTARGUMENT'                                           => 'input-hidden',
-	                'ctl00_cphMain_sManager_HiddenField'                        => 'input-hidden',
-	                'VAM_JSE'                                                   => 'input-hidden',
-	                'VAM_Group'                                                 => 'input-hidden',
-	                '__VIEWSTATE'                                               => 'input-hidden',
-	                '__EVENTVALIDATION'                                         => 'input-hidden',
-	                'ctl00$cphMain$favoriteSearchesList$ddlFavorites'           => 'select',
-	                'ctl00$cphMain$favoriteSearchesList$favoritePostBackTarget' => 'input-hidden',
-	                'ctl00$cphMain$recentSearchesList$ddlRecentSearches'        => 'select',
-	                'ctl00$cphMain$recentSearchesList$recentPostBackTarget'     => 'input-hidden',
-	                'ctl00$cphMain$ddcTruckType$txtEntry'                       => 'input-text',
-	                'ctl00$cphMain$locOrigin$txtLocationEntry'                  => 'input-text',
-	                'ctl00$cphMain$locOrigin$btnCityRegionDirOpen'              => 'input-button',
-	                'ctl00$cphMain$locOrigin$hdnNGL'                            => 'input-hidden',
-	                'ctl00$cphMain$txtOriginRadius'                             => 'input-text',
-	                'ctl00$cphMain$locDestination$txtLocationEntry'             => 'input-text',
-	                'ctl00$cphMain$locDestination$btnCityRegionDirOpen'         => 'input-button',
-	                'ctl00$cphMain$locDestination$hdnNGL'                       => 'input-hidden',
-	                'ctl00$cphMain$txtDestinationRadius'                        => 'input-text',
-	                'ctl00$cphMain$ddlLoadType'                                 => 'select',
-	                'ctl00$cphMain$txtLength'                                   => 'input-text',
-	                'ctl00$cphMain$txtWeight'                                   => 'input-text',
-	                'ctl00$cphMain$txtDateFrom'                                 => 'input-text',
-	                'ctl00$cphMain$txtDateTo'                                   => 'input-text',
-	                'ctl00$cphMain$txtAge'                                      => 'input-text',
-	                'ctl00$cphMain$btnSearch'                                   => 'input-submit',
-	                'ctl00$cphMain$btnClear'                                    => 'input-button',
-	                ));
-				
-	$client->removeField('ctl00$cphMain$btnClear');
-	$tag['search'] = $client->getData();
-	$tag['search']['VAM_JSE'] = 1;
-	$tag['search']['ctl00$cphMain$locDestination$hdnNGL'] = '';
-	$tag['search']['ctl00$cphMain$txtAge'] = $config->max_age;
-	$tag['search']['ctl00$cphMain$locOrigin$txtLocationEntry'] = $config->origin;
-	$tag['search']['ctl00$cphMain$txtOriginRadius'] = $config->origin_radius;
-	$tag['search']['ctl00$cphMain$locDestination$txtLocationEntry'] = $config->destination;
-	$tag['search']['ctl00$cphMain$txtDestinationRadius'] = $config->destination_radius;
-	$tag['search']['ctl00$cphMain$txtDateFrom'] = date('d/m/y', strtotime($config->from_date));
-	$tag['search']['ctl00$cphMain$txtDateTo'] = date('d/m/y', strtotime($config->to_date));
-	$tag['search']['ctl00$cphMain$locOrigin$hdnNGL'] = '';
-	$tag['search']['ctl00$cphMain$ddlLoadType'] = $this->mapping($config->loads_type, array( '0'  =>  'Both',
-																							 '1'  =>  'Full',
-																							 '2'  =>  'Partial'));
-	$client->fill($tag['search']);
-	$client->post('http://www.truckersedge.net/a/app/Search.aspx');
-	$this->create_log('step1'.date(DATE_ISO8601).'.html', $client->getBody());
-	// parsing reponse
-	
-	$doc = new DOMDocument();
-    @$doc->loadHTML($client->getBody());
-    $xpath = new DOMXpath($doc);
-
-    $nodes = $xpath->query('//*[@id="resultSet"]/tbody/tr');
-	foreach ($nodes as $node) {
-		$tds = $xpath->query('td', $node);
-		$items = array();
-        foreach ($tds as $td)
-            $items[] = trim($td->nodeValue);
-		$this->addLoads($items);
-	}
+	$generator = new TruckersedgeGenerator($config_id, 'Truckersedge');
+	$generator->execute();
+	$loads = $generator->getLoads();
+	foreach ($loads as $item)
+		$this->addLoads($item);
   }
 
 
@@ -168,14 +64,6 @@ class TruckersedgeGeneratorTask extends sfBaseTask
 			$loads->save();
 		}
 	}
-  }
-
-
-  private function mapping($value, $mapping)
-  {
-	if (array_key_exists($value, $mapping))
-		return $mapping[$value];
-	else return null;
   }
 }
 ?>
