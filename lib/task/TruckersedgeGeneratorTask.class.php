af16e1edbc42a48a8c9df20b7af5996ea76f932d
letuananha3 <letuananha3@gmail.com>
2012-06-20 08:42:33 +0000

    using american MST time zone

--- lib/task/TruckersedgeGeneratorTask.class.php
+++ lib/task/TruckersedgeGeneratorTask.class.php
@@ -21,7 +21,7 @@ class TruckersedgeGeneratorTask extends sfBaseTask
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-    date_default_timezone_set('Asia/Bangkok');
+    date_default_timezone_set('America/Phoenix');
     $client = new WebFormClient();
 
     // getting config
