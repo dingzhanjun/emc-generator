2aab7cb5fd4f968eb9015ccb3a5a02be40a99116
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-20 08:37:58 +0000

    update tab with quick search icon

--- trunk/apps/frontend/modules/default/templates/_tabs.php
+++ trunk/apps/frontend/modules/default/templates/_tabs.php
@@ -2,7 +2,7 @@
 <div id='left_tabs'>
     <a href='<?php echo url_for('@config'); ?>' id='below_grounds_manager' class='below_tab_icon <?php echo ($sf_context->getModuleName() == 'config')?"active":"" ?>'><img src='/images/icon_system.png' height="96" /><div class='below_tab_label'>Config</div></a>
     <a href='<?php echo url_for('@loads'); ?>' id='below_users_manager' class='below_tab_icon <?php echo ($sf_context->getModuleName() == 'loads')?"active":"" ?>'><img src='/images/icon_trucks.png' height="96" /><div class='below_tab_label'>Loads</div></a>
-    <a href='<?php //echo url_for('@ground_order'); ?>' id='below_services_manager' class='below_tab_icon <?php // echo ($sf_context->getModuleName() == 'groundOrder')?"active":"" ?>'><img src='/images/icon_search.png' height="96" /><div class='below_tab_label'>Quick Search</div></a>
+    <a href='<?php echo url_for('@quick_search'); ?>' id='below_services_manager' class='below_tab_icon <?php echo ($sf_context->getModuleName() == 'quickSearch')?"active":"" ?>'><img src='/images/icon_search.png' height="96" /><div class='below_tab_label'>Quick Search</div></a>
 <?
 /*
     <a href='<?php //echo url_for('@cms_category_list'); ?>' id='below_cms_manager' class='below_tab_icon <?php echo ($sf_context->getModuleName() == 'cms')?"active":"" ?>'><img src='/images/icon_news.png' height="96" /><div class='below_tab_label'>Tin tức</div></a>
