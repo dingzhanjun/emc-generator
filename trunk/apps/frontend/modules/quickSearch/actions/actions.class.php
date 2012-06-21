7a5f58252221b68e12bf82017cbe580b72b50ad8
letuananha3@gmail.com <letuananha3@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-06-21 01:56:50 +0000

    use spaces instead of tabs

--- trunk/apps/frontend/modules/quickSearch/actions/actions.class.php
+++ trunk/apps/frontend/modules/quickSearch/actions/actions.class.php
@@ -18,83 +18,83 @@ class quickSearchActions extends sfActions
   public function executeIndex(sfWebRequest $request)
   {
     $this->search_form = new ConfigForm();
-	if ($request->hasParameter('config')) {
-		$form_data = $request->getParameter('config');
-		$this->search_form->bind($form_data);
-		if ($this->search_form->isValid()) {
-			$config = new Config();
-			$config->type = 1; // TODO put a constant here
-			$config->save();
-			$this->updateConfig($form_data, $config);
-			
-			// process
-			$this->loads = array();
-			$jobboard_configs = Doctrine_Query::create()->from('JobboardConfig jc')->addWhere('jc.config_id = ?', $config->id)->execute();
-			foreach ($jobboard_configs as $jobboard_config) {
-				$jobboard = $jobboard_config->Jobboard;
-				$generator_name = $jobboard->name.'Generator';
-				$generator = new $generator_name($config->id, $jobboard->name);
-				if ($generator) {
-					$generator->execute();
-					$this->loads[$jobboard->name] = $generator->getLoads();
-				}
-			}
-			$this->setlayout(false);
-		    $this->getResponse()->setHttpHeader('Content-Type', 'text/csv');
-		    $this->getResponse()->setHttpHeader('Content-Disposition', 'attachment; filename=quick_search ' . date("Y-m-d_Hi") . '.csv');
-		    return 'CSV';
-		}	
-	}
-	return sfView::SUCCESS;
+    if ($request->hasParameter('config')) {
+        $form_data = $request->getParameter('config');
+        $this->search_form->bind($form_data);
+        if ($this->search_form->isValid()) {
+            $config = new Config();
+            $config->type = 1; // TODO put a constant here
+            $config->save();
+            $this->updateConfig($form_data, $config);
+            
+            // process
+            $this->loads = array();
+            $jobboard_configs = Doctrine_Query::create()->from('JobboardConfig jc')->addWhere('jc.config_id = ?', $config->id)->execute();
+            foreach ($jobboard_configs as $jobboard_config) {
+                $jobboard = $jobboard_config->Jobboard;
+                $generator_name = $jobboard->name.'Generator';
+                $generator = new $generator_name($config->id, $jobboard->name);
+                if ($generator) {
+                    $generator->execute();
+                    $this->loads[$jobboard->name] = $generator->getLoads();
+                }
+            }
+            $this->setlayout(false);
+            $this->getResponse()->setHttpHeader('Content-Type', 'text/csv');
+            $this->getResponse()->setHttpHeader('Content-Disposition', 'attachment; filename=quick_search ' . date("Y-m-d_Hi") . '.csv');
+            return 'CSV';
+        }   
+    }
+    return sfView::SUCCESS;
   }
 
 
   private function updateConfig($form, $config)
   {
-	$config->max_age = $form['max_age'];
-	$config->origin = $form['origin'];
-	$config->origin_radius = $form['origin_radius'];
-	$config->destination = $form['destination'];
-	$config->destination_radius = $form['destination_radius'];
-	$config->loads_type = $form['loads_type'];
-	$config->length = $form['length'];
-	$config->weight = $form['weight'];
-	$config->from_date = date('d/m/y', strtotime($form['from_date']));
-	$config->to_date = date('d/m/y', strtotime($form['to_date']));
-	$config->save();
+    $config->max_age = $form['max_age'];
+    $config->origin = $form['origin'];
+    $config->origin_radius = $form['origin_radius'];
+    $config->destination = $form['destination'];
+    $config->destination_radius = $form['destination_radius'];
+    $config->loads_type = $form['loads_type'];
+    $config->length = $form['length'];
+    $config->weight = $form['weight'];
+    $config->from_date = date('d/m/y', strtotime($form['from_date']));
+    $config->to_date = date('d/m/y', strtotime($form['to_date']));
+    $config->save();
 
-	// jobboard config
-	$jobboard_ids = $form['jobboard_id'];
-	$jobboard_configs = $config->JobboardConfigs;
-	if (!empty($jobboard_configs))
-		foreach ($jobboard_configs as $jobboard_config)
-			$jobboard_config->delete();
+    // jobboard config
+    $jobboard_ids = $form['jobboard_id'];
+    $jobboard_configs = $config->JobboardConfigs;
+    if (!empty($jobboard_configs))
+        foreach ($jobboard_configs as $jobboard_config)
+            $jobboard_config->delete();
 
-	if ($jobboard_ids[0] == 'all') {
-		$jobboards = Doctrine_Query::create()->from('Jobboard j')->execute();
-		$jobboard_ids = array();
-		foreach ($jobboards as $jobboard)
-			$jobboard_ids[] = $jobboard->id;
-	}
+    if ($jobboard_ids[0] == 'all') {
+        $jobboards = Doctrine_Query::create()->from('Jobboard j')->execute();
+        $jobboard_ids = array();
+        foreach ($jobboards as $jobboard)
+            $jobboard_ids[] = $jobboard->id;
+    }
 
-	foreach ($jobboard_ids as $jobboard_id) {
-		$jobboard_config = new JobboardConfig();
-		$jobboard_config->jobboard_id = $jobboard_id;
-		$jobboard_config->config_id = $config->id;
-		$jobboard_config->save();
-	}
+    foreach ($jobboard_ids as $jobboard_id) {
+        $jobboard_config = new JobboardConfig();
+        $jobboard_config->jobboard_id = $jobboard_id;
+        $jobboard_config->config_id = $config->id;
+        $jobboard_config->save();
+    }
 
-	// truck types
-	$truck_types = $form['truck_type'];
-	$config_trucks = $config->ConfigTrucks;
-	if (!empty($config_trucks))
-	 	foreach ($config_trucks as $config_truck)
-			$config_truck->delete();
-	foreach ($truck_types as $truck_id) {
-		$config_truck = new ConfigTruck();
-		$config_truck->truck_id = $truck_id;
-		$config_truck->config_id = $config->id;
-		$config_truck->save();
-	}
+    // truck types
+    $truck_types = $form['truck_type'];
+    $config_trucks = $config->ConfigTrucks;
+    if (!empty($config_trucks))
+        foreach ($config_trucks as $config_truck)
+            $config_truck->delete();
+    foreach ($truck_types as $truck_id) {
+        $config_truck = new ConfigTruck();
+        $config_truck->truck_id = $truck_id;
+        $config_truck->config_id = $config->id;
+        $config_truck->save();
+    }
   }
 }
