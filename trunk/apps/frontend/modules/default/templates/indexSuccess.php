f77890632ffe1dbbefc6556daf2dc3697602355e
truongkyanh@gmail.com <truongkyanh@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-07-01 21:17:45 +0000

    add link for top tabs

--- trunk/apps/frontend/modules/default/templates/indexSuccess.php
+++ trunk/apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,8 +1,8 @@
 <div id="signout"><a href="#"><img src='/images/logout.png' onclick="sign_out()" /></a></div>
 <div id='main_tabs'>
-	<a rel='<?php echo url_for('@config'); ?>' id='grounds_manager' class='tab_icon'><img src='/images/icon_system.png' /><div class='tab_label'>Config</div></a>
-    <a rel='<?php echo url_for('@loads'); ?>' id='users_manager' class='tab_icon'><img src='/images/icon_trucks.png' /><div class='tab_label'>Loads</div></a>
-    <a rel='<?php //echo url_for('@ground_order'); ?>' id='services_manager' class='tab_icon'><img src='/images/icon_search.png' /><div class='tab_label'>Quick Search</div></a>
+	<a href='<?php echo url_for('@config'); ?>' rel='<?php echo url_for('@config'); ?>' id='grounds_manager' class='tab_icon'><img src='/images/icon_system.png' /><div class='tab_label'>Config</div></a>
+    <a href='<?php echo url_for('@loads'); ?>' rel='<?php echo url_for('@loads'); ?>' id='users_manager' class='tab_icon'><img src='/images/icon_trucks.png' /><div class='tab_label'>Loads</div></a>
+    <a href='<?php echo url_for('@quick_search'); ?>' rel='<?php //echo url_for('@ground_order'); ?>' id='services_manager' class='tab_icon'><img src='/images/icon_search.png' /><div class='tab_label'>Quick Search</div></a>
 <?
 /*
     <a rel='<?php //echo url_for('@cms_category_list'); ?>' id='cms_manager' class='tab_icon'><img src='/images/icon_news.png' /><div class='tab_label'>Tin tức</div></a>
