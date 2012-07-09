1628c6f316988040eb1ecdbe36f0f1f4b1c06768
truongkyanh@gmail.com <truongkyanh@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-07-09 11:02:47 +0000

    add field alias for table Jobboard.

--- trunk/data/sql/migrate.sql
+++ trunk/data/sql/migrate.sql
@@ -1,2 +1,8 @@
 /* One time you execute this scripts on working server pls put the codes to comment lines so we can follow the database's changes */
-INSERT INTO jobboard VALUES (null, 'Truckstop', 'TruckstopGeneratorTask', 'http://truckstop.com/lite', 't193269rzps', 'wat2re5you', now(), now());
+/* INSERT INTO jobboard VALUES (null, 'Truckstop', 'TruckstopGeneratorTask', 'http://truckstop.com/lite', 't193269rzps', 'wat2re5you', now(), now()); */
+ALTER TABLE `jobboard` ADD COLUMN `alias` VARCHAR(255) NOT NULL AFTER `id`;
+UPDATE `jobboard` SET alias = 'TE' WHERE id = 1
+UPDATE `jobboard` SET alias = 'FV' WHERE id = 2
+UPDATE `jobboard` SET alias = 'GF' WHERE id = 3
+UPDATE `jobboard` SET alias = 'LS' WHERE id = 4
+UPDATE `jobboard` SET alias = 'TS' WHERE id = 5
