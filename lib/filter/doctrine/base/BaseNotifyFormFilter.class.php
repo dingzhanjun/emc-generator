799956ed27c08f9b1db9cd3f366c4a29686cb7e2
truongkyanh <truongkyanh@gmail.com>
2012-07-12 04:16:34 +0000

    update filter, form, model class

--- lib/filter/doctrine/base/BaseNotifyFormFilter.class.php
+++ lib/filter/doctrine/base/BaseNotifyFormFilter.class.php
@@ -1,30 +1,32 @@
 <?php
 
 /**
- * Generator filter form base class.
+ * Notify filter form base class.
  *
  * @package    emc
  * @subpackage filter
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormFilterGeneratedTemplate.php 29570 2010-05-21 14:49:47Z Kris.Wallsmith $
  */
-abstract class BaseGeneratorFormFilter extends BaseFormFilterDoctrine
+abstract class BaseNotifyFormFilter extends BaseFormFilterDoctrine
 {
   public function setup()
   {
     $this->setWidgets(array(
-      'name'       => new sfWidgetFormFilterInput(array('with_empty' => false)),
+      'content'    => new sfWidgetFormFilterInput(),
+      'status'     => new sfWidgetFormChoice(array('choices' => array('' => 'yes or no', 1 => 'yes', 0 => 'no'))),
       'created_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
       'updated_at' => new sfWidgetFormFilterDate(array('from_date' => new sfWidgetFormDate(), 'to_date' => new sfWidgetFormDate(), 'with_empty' => false)),
     ));
 
     $this->setValidators(array(
-      'name'       => new sfValidatorPass(array('required' => false)),
+      'content'    => new sfValidatorPass(array('required' => false)),
+      'status'     => new sfValidatorChoice(array('required' => false, 'choices' => array('', 1, 0))),
       'created_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
       'updated_at' => new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 00:00:00')), 'to_date' => new sfValidatorDateTime(array('required' => false, 'datetime_output' => 'Y-m-d 23:59:59')))),
     ));
 
-    $this->widgetSchema->setNameFormat('generator_filters[%s]');
+    $this->widgetSchema->setNameFormat('notify_filters[%s]');
 
     $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
 
@@ -35,14 +37,15 @@ abstract class BaseGeneratorFormFilter extends BaseFormFilterDoctrine
 
   public function getModelName()
   {
-    return 'Generator';
+    return 'Notify';
   }
 
   public function getFields()
   {
     return array(
-      'id'         => 'Number',
-      'name'       => 'Text',
+      'notify_id'  => 'Number',
+      'content'    => 'Text',
+      'status'     => 'Boolean',
       'created_at' => 'Date',
       'updated_at' => 'Date',
     );
