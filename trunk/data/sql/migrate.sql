0318412c42f1c20f7c5e2553a56df26c1a4bad46
truongkyanh@gmail.com <truongkyanh@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-07-12 04:00:01 +0000

    update migrate, schema

--- trunk/data/sql/migrate.sql
+++ trunk/data/sql/migrate.sql
@@ -6,3 +6,5 @@ UPDATE `jobboard` SET alias = 'FV' WHERE id = 2;
 UPDATE `jobboard` SET alias = 'GF' WHERE id = 3;
 UPDATE `jobboard` SET alias = 'LS' WHERE id = 4;
 UPDATE `jobboard` SET alias = 'TS' WHERE id = 5;
+
+CREATE TABLE notify (notify_id BIGINT AUTO_INCREMENT, content VARCHAR(255), status TINYINT(1) DEFAULT '0' NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(notify_id)) ENGINE = INNODB;
