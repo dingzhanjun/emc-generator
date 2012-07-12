505aec9d085370d80bd7324b37499b7fe1380308
truongkyanh@gmail.com <truongkyanh@gmail.com@076c01a9-ba5e-5c9f-bbcb-42123c9c02aa>
2012-07-12 04:12:55 +0000

    Add notify generators errors to notice customers

--- trunk/lib/generator/Gofmi/GofmiGenerator.class.php
+++ trunk/lib/generator/Gofmi/GofmiGenerator.class.php
@@ -41,82 +41,88 @@ class GofmiGenerator
 
 		$config = Doctrine_Core::getTable('Config')->find($this->config_id);
 		if (!$config) {
-			echo ">>>>>Error<<<<< Config not found\n";
-			exit;
+			$notify_error = new NotifyError("Gofmi - Config not found\n");
+			$notify_error->execute();
+			return;
 		}
 		
 		$jobboard = Doctrine_Core::getTable('Jobboard')->findOneByName($this->jobboard_name);
 		if (!$jobboard) {
-			echo ">>>>>Error<<<<< Jobboard not found\n";
-			exit;
+			$notify_error = new NotifyError("Gofmi - Jobboard not found\n");
+			$notify_error->execute();
+			return;
 		}
 		
-		// ok everything is ready, lets go
-		$tag = array();
-		$base_url = $jobboard->address;
-		$client->get($base_url);
-
-		// parsing reponse
-		$config_trucks = Doctrine_Query::create()
-		    ->from('ConfigTruck cf')
-		    ->addWhere('cf.config_id = ?', $config->id)
-		    ->execute();
-		$trucks = array();
-		foreach ($config_trucks as $config_truck) {
-		    switch ($config_truck->truck_id) {
-		        case 1:
-		         $trucks[] = 'Dry Bulk';
-		         break;
-		        case 2:
-		         $trucks[] = 'Container';
-		         break;
-		        case 3:
-		         $trucks[] = 'Deck';
-		         break;
-		        case 4:
-		         $trucks[] = 'Flatbed';
-		         break;
-		        case 7:
-		         $trucks[] = 'Reefer';
-		         break;
-		        case 8:
-		         $trucks[] = 'Van';
-		         break;
-		        case 9:
-		         $trucks[] = 'Tanker';
-		         break;
-		    }
+		try {
+			// ok everything is ready, lets go
+			$tag = array();
+			$base_url = $jobboard->address;
+			$client->get($base_url);
+	
+			// parsing reponse
+			$config_trucks = Doctrine_Query::create()
+				->from('ConfigTruck cf')
+				->addWhere('cf.config_id = ?', $config->id)
+				->execute();
+			$trucks = array();
+			foreach ($config_trucks as $config_truck) {
+				switch ($config_truck->truck_id) {
+					case 1:
+					 $trucks[] = 'Dry Bulk';
+					 break;
+					case 2:
+					 $trucks[] = 'Container';
+					 break;
+					case 3:
+					 $trucks[] = 'Deck';
+					 break;
+					case 4:
+					 $trucks[] = 'Flatbed';
+					 break;
+					case 7:
+					 $trucks[] = 'Reefer';
+					 break;
+					case 8:
+					 $trucks[] = 'Van';
+					 break;
+					case 9:
+					 $trucks[] = 'Tanker';
+					 break;
+				}
+			}
+	
+			$doc = new DOMDocument();
+			@$doc->loadHTML($client->getBody());
+			$xpath = new DOMXpath($doc);
+	
+			$nodes = $xpath->query('//*[@id="wooMod"]/table/tbody/tr');
+			foreach ($nodes as $node) {
+				$tds = $xpath->query('td', $node);
+				$items = array();
+				foreach ($tds as $td)
+					$items[] = trim($td->nodeValue);
+				$in_config = true;
+				
+				$type_is_ok = false;
+				foreach (explode(" ", $items[2]) as $truck) {
+					if (in_array($truck, $trucks))
+						$type_is_ok = true;
+				}
+						
+				$loads_type_is_ok = false;
+				if ($config->loads_type != 0) {
+					if (($config->loads_type == 1 && $items[5] == 'Full') || ($config->loads_type == 2 && $items[5] == 'Partial'))
+						$loads_type_is_ok = true;
+				} else $loads_type_is_ok = true;
+				
+				$in_config = $type_is_ok && $loads_type_is_ok;
+				if ($in_config)
+					$this->addLoads($items);
+			}
+		} catch (Exception $ex) {
+			$notify_error = new NotifyError("Gofmi - Jobboard have been changed. Please contact to VTNS\n");
+			$notify_error->execute();
 		}
-
-		$doc = new DOMDocument();
-	    @$doc->loadHTML($client->getBody());
-	    $xpath = new DOMXpath($doc);
-
-	    $nodes = $xpath->query('//*[@id="wooMod"]/table/tbody/tr');
-		foreach ($nodes as $node) {
-			$tds = $xpath->query('td', $node);
-			$items = array();
-	        foreach ($tds as $td)
-	            $items[] = trim($td->nodeValue);
-	        $in_config = true;
-	        
-	        $type_is_ok = false;
-	        foreach (explode(" ", $items[2]) as $truck) {
-	            if (in_array($truck, $trucks))
-	                $type_is_ok = true;
-	        }
-	                
-	        $loads_type_is_ok = false;
-	        if ($config->loads_type != 0) {
-	            if (($config->loads_type == 1 && $items[5] == 'Full') || ($config->loads_type == 2 && $items[5] == 'Partial'))
-	                $loads_type_is_ok = true;
-	        } else $loads_type_is_ok = true;
-	        
-	        $in_config = $type_is_ok && $loads_type_is_ok;
-	        if ($in_config)
-			    $this->addLoads($items);
-		}
-		
 	}
 	
 	
